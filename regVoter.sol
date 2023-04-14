4c19213075fc9e7d76a7417c8cf28dabdd9630c0
Constantino <Okonemmanuelmma@gmail.com>
2023-04-14 21:12:02 +0100

    #feature: voter struct defined

---
+++ regVoter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Voter{
+
+    struct voter{
+        string name;
+        string age;
+        string votingCenter;
+        address userAddress;
+        string accreditation;
+    }
+
+}
